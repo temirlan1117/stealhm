593e0be3712497a192b87766cb52b3d9c928434d
bekkkks <ewefe811@gmail.com>
2022-12-19 17:08:54 +0600

    Амиру.

---
+++ lesson/less5.py
@@ -0,0 +1,20 @@
+# 1 встроенный
+# import math
+# p=9
+# print(math.sqrt(p))
+# from math import pi as aldiyar
+# print(math.pi)
+# print(aldiyar)
+#
+# # 2 свои модули
+# import lesson4
+# from lesson4 import Emirlan
+#
+# a=Emirlan('Байэль',12)
+# a.run()
+# 3 взаимствованные чужие
+import colorama
+
+print(colorama.Fore.MAGENTA)
+print("ghjk")
+print(colorama.Style.BRIGHT)
